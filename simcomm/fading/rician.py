e33c6dcc16f7ddac0153fc6b4896bde6d3c413cd
muhd-umer <thisismumer@gmail.com>
2023-07-02 21:06:30 +0500

    Added link argument

--- simcomm/fading/rician.py
+++ simcomm/fading/rician.py
@@ -7,7 +7,7 @@ import numpy as np
 from .rayleigh import *
 
 
-def rician_fading(K, P_los, n=1):
+def rician_fading(K, P_los, n=1, link=True):
     """
     Generate the Rician fading channel coefficients.
 
@@ -22,9 +22,14 @@ def rician_fading(K, P_los, n=1):
 
     s = np.sqrt(K / (K + 1) * P_los)  # Non-centrality parameter
     sigma = P_los / np.sqrt(2 * (K + 1))  # Standard deviation
-    los = np.sqrt(
-        (np.random.normal(s, sigma) ** 2) + 1j * (np.random.normal(0, sigma) ** 2)
-    )
-    nlos = rayleigh_fading(n)
-    h = np.sqrt(K / (K + 1)) * los + np.sqrt(1 / (K + 1)) * nlos
+    if link:
+        los = np.sqrt(
+            (np.random.normal(s, sigma) ** 2) + 1j * (np.random.normal(0, sigma) ** 2)
+        )
+        nlos = rayleigh_fading(n)
+        h = np.sqrt(K / (K + 1)) * los + np.sqrt(1 / (K + 1)) * nlos
+    else:
+        h = np.sqrt(
+            (np.random.normal(s, sigma) ** 2) + 1j * (np.random.normal(0, sigma) ** 2)
+        )
     return h
