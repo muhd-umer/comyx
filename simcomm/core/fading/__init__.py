c40021132964449b0c87141472973b8b083889b6
muhd-umer <thisismumer@gmail.com>
2023-07-16 01:16:33 +0500

    Implemented fading distributions

--- simcomm/core/fading/__init__.py
+++ simcomm/core/fading/__init__.py
@@ -2,26 +2,37 @@ from .rayleigh import *
 from .rician import *
 
 
-def get_fading(type, *args, **kwargs):
+def get_multipath_fading(type, size, *args, **kwargs):
     """
-    Generates the fading channel coefficients.
+    Generates the multipath fading coefficients.
 
     Args:
         type: The type of the fading. ("rayleigh", "rician")
-        distance: The distance between the transmitter and receiver.
-        eta: The path loss exponent.
-        *args: The arguments to pass to the channel generator.
-        **kwargs: The keyword arguments to pass to the channel generator.
+        size: The number of fading coefficients to generate.
+        library: The library to use for generating the fading coefficients.
+
+    Rayleigh Args:
+        sigma: The scale parameter of the Rayleigh distribution.
+
+    Rician Args:
+        K: The shape parameter of the Rician distribution.
+        param: Can be either the scale parameter or the non-centrality parameter.
+               [Scale] omega: The scale parameter of the Rician distribution.
+               [Variance] sigma: The variance of the Rician distribution.
 
     Returns:
         A NumPy array of complex numbers representing the fading coefficients.
     """
 
     if type == "rayleigh":
-        small_scale_fading = rayleigh_fading(*args, **kwargs)
+        distribution = Rayleigh(*args, **kwargs)
+        coefficients = distribution.generate(size=size)
+
     elif type == "rician":
-        small_scale_fading = rician_fading(*args, **kwargs)
+        distribution = Rician(*args, **kwargs)
+        coefficients = distribution.generate(size=size)
+
     else:
         raise NotImplementedError(f"Channel type {type} is not implemented")
 
-    return small_scale_fading
+    return coefficients
