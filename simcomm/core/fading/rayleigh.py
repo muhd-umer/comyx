abe9966a3e1378539ae00fe42e4cb51e6c84202c
muhd-umer <thisismumer@gmail.com>
2023-07-16 01:16:33 +0500

    Implemented fading distributions

--- simcomm/core/fading/rayleigh.py
+++ simcomm/core/fading/rayleigh.py
@@ -3,19 +3,61 @@ Implementation of Rayleigh fading channel.
 """
 
 import numpy as np
+import scipy.stats as stats
 
 
-def rayleigh_fading(n=1):
+class Rayleigh:
     """
-    Generates a Rayleigh fading coefficient as a complex Gaussian
-    random variable with zero mean and unit variance.
+    A class representing a Rayleigh distribution.
 
-    Args:
-        n: The number of samples to generate. Defaults to 1.
+    Properties
+    - Density function := f(x) = (x / sigma^2) * exp(-x^2 / (2 * sigma^2))
+    - Expected value := sigma * sqrt(pi / 2)
+    - Variance := (2 - pi / 2) * sigma^2
+    - RMS value := sqrt(2) * sigma
 
-    Returns:
-        A NumPy array of complex numbers representing the fading coefficients.
+    Attributes:
+        sigma: The scale parameter of the Rayleigh distribution.
+
+    Reference:
+        https://en.wikipedia.org/wiki/Rayleigh_distribution
     """
 
-    h = (np.random.randn(n) + 1j * np.random.randn(n)) / np.sqrt(2)
-    return h
+    def __init__(self, sigma):
+        self.sigma = sigma
+
+    def pdf(self, x):
+        """
+        Return the probability density function of the Rayleigh distribution.
+        """
+        return (x / self.sigma**2) * np.exp(-(x**2) / (2 * self.sigma**2))
+
+    def cdf(self, x):
+        """
+        Return the cumulative distribution function of the Rayleigh distribution.
+        """
+        return 1 - np.exp(-(x**2) / (2 * self.sigma**2))
+
+    def expected_value(self):
+        """
+        Return the expected value of the Rayleigh distribution.
+        """
+        return self.sigma * np.sqrt(np.pi / 2)
+
+    def variance(self):
+        """
+        Return the variance of the Rayleigh distribution.
+        """
+        return (2 - np.pi / 2) * self.sigma**2
+
+    def rms_value(self):
+        """
+        Return the RMS value of the Rayleigh distribution.
+        """
+        return np.sqrt(2) * self.sigma
+
+    def generate(self, size):
+        """
+        Generate random variables from the Rayleigh distribution.
+        """
+        return stats.rayleigh.rvs(loc=0, scale=self.sigma, size=size)
