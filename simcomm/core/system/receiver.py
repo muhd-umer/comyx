Fixed outage probability computation

simcomm/core/system/receiver.py
@@ -2,7 +2,7 @@ from typing import List
 
 import numpy as np
 
-from ...utils import qfunc
+from ...utils import pow2db, qfunc
 from .system import SystemObject
 
 
@@ -11,9 +11,7 @@ class Receiver(SystemObject):
 
     Args:
         sensitivity (float): The sensitivity of the receiver.
-        margin (float): The margin of the receiver.
         rate (float): The rate of the receiver.
-        snr (float): The signal-to-noise ratio of the receiver.
         outage (float): The outage probability of the receiver.
 
     Attributes:
@@ -22,27 +20,16 @@ class Receiver(SystemObject):
                                 [x, y, z] coordinates if 3D.
     """
 
-    def __init__(
-        self, name: str, position: List[float], margin: float, sensitivity: float
-    ) -> None:
+    def __init__(self, name: str, position: List[float], sensitivity: float) -> None:
         """
         Initializes a Receiver object with the given parameters.
 
         Args:
             name: The name of the receiver.
             position: The position of the receiver in 2D or 3D space.
-            margin: The margin of the receiver.
             sensitivity: The sensitivity of the receiver.
-
-        Attributes:
-            name (str): The name of the receiver.
-            position (list): The position of the receiver in 2D or 3D space.
-            margin (float): The margin of the receiver.
-            rate (float): The rate of the receiver.
-            outage (float): The outage probability of the receiver.
         """
         super().__init__(name, position)
-        self.margin = margin
         self.sensitivity = sensitivity
         self.rate = None
         self.outage = None
@@ -132,5 +119,7 @@ class Receiver(SystemObject):
             sigma: Shadowing standard deviation.
         """
         self.outage = qfunc(
-            np.mean((self.snr + noise_power - self.sensitivity) / sigma, axis=0)
+            np.mean(
+                (pow2db(self.snr) - (-noise_power) - self.sensitivity) / sigma, axis=0
+            )
         )