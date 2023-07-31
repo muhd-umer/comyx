2b598bd392432a1eb97b7fd0e56893df8aed5014
muhd-umer <thisismumer@gmail.com>
2023-08-01 00:56:32 +0500

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,21 +10,36 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
+
+# Package information
+import simcomm
+
+version = simcomm.__version__
+
 project = "SimComm"
 copyright = "2023, Muhammad Umer"
 author = "Muhammad Umer"
-release = "0.0.1"
+release = str(version)
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
-    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_copybutton",
+    "sphinx_design",
 ]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy-1.8.1/", None),
+}
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -36,3 +51,14 @@ exclude_patterns = []
 html_theme = "pydata_sphinx_theme"
 html_title = "SimComm"
 html_static_path = ["_static"]
+html_show_sourcelink = False
+html_show_sphinx = False
+
+html_theme_options = {
+    "show_toc_level": 4,
+    "github_url": "https://github.com/muhd-umer/simcomm",
+}
+
+html_css_files = [
+    "simcomm.css",
+]
