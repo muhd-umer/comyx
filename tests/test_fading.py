5e77c9aa409ef82c034b3a9dac04fbebfdbf05b8
muhd-umer <thisismumer@gmail.com>
2023-08-01 19:19:38 +0500

    Created tests/

---
+++ tests/test_fading.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+# Append the path depending on where the script is executed
+if os.path.basename(os.getcwd()) == "simulations":
+    sys.path.append("..")
+elif os.path.basename(os.getcwd()) == "simcomm":
+    sys.path.append(".")
+else:
+    raise Exception(
+        "Please execute this script from either simcomm/ or simulations/ folder."
+    )
+
+from unittest import TestCase, main
+
+import numpy as np
+
+from simcomm.core.fading import get_rvs
+
+
+class TestGetRvs(TestCase):
+    def test_rayleigh(self):
+        # Test generating 1 coefficient
+        result = get_rvs("rayleigh", 1, sigma=1)
+        self.assertEqual(result.shape, (1,))
+        self.assertGreaterEqual(result[0], 0)
+
+        # Test generating 5 coefficients
+        result = get_rvs("rayleigh", 5, sigma=1)
+        self.assertEqual(result.shape, (5,))
+        self.assertTrue(np.all(result >= 0))
+
+    def test_rician(self):
+        # Test generating 1 coefficient
+        result = get_rvs("rician", 1, K=0, sigma=1)
+        self.assertEqual(result.shape, (1,))
+        self.assertGreaterEqual(result[0], 0)
+
+        # Test generating 5 coefficients
+        result = get_rvs("rician", 5, K=0, sigma=1)
+        self.assertEqual(result.shape, (5,))
+        self.assertTrue(np.all(result >= 0))
+
+    def test_invalid_type(self):
+        # Test generating with invalid type
+        with self.assertRaises(NotImplementedError):
+            get_rvs("invalid_type", 1, sigma=1)
+
+
+if __name__ == "__main__":
+    main()
