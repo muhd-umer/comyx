cf749de2eabc00f29b493a4c09a845c717895e1c
muhd-umer <thisismumer@gmail.com>
2024-01-16 02:15:38 +0500

    Refactor SystemObject class to improve code readability

--- comyx/core/system/system.py
+++ comyx/core/system/system.py
@@ -2,26 +2,18 @@ from typing import List
 
 
 class SystemObject:
-    """Base class for all system objects in the network except for the channel, as it is not a physical object.
+    """Base class for all system objects in the network except for the channel.
 
     Args:
         name: The name of the system object.
         position: The [x, y] or [x, y, z] coordinates of the system object.
-
-    Attributes:
-        name: Stores the name of the system object.
-        position : Stores the [x, y] or [x, y, z] coordinates of the system object.
     """
 
     def __init__(self, name: str, position: List[float]):
-        """Initializes a SystemObject object with the given parameters.
-
-        Args:
-            name: The name of the system object.
-            position: The [x, y] or [x, y, z] coordinates of the system object.
-        """
+        """Initializes a SystemObject object with the given parameters."""
         self.name = name
         self.position = position
 
     def __str__(self):
-        return f"{self.name} at {self.position}"
+        """Returns a string representation of the SystemObject."""
+        return f"SystemObject(name={self.name}, position={self.position})"
